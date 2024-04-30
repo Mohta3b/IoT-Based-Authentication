2b09103a3b5245d812f818bb0863df53ee2801c9
Shayan Shahgholi <amirhossein.shahgholi@divar.ir>
2024-04-30 23:53:57 +0330

    Some minor message style change

--- Client/src/cpsapplication.cpp
+++ Client/src/cpsapplication.cpp
@@ -51,7 +51,7 @@ void Application::showHistoryWindow()
 {
     setWindowsThemeToDark<HistoryWindow>(*_history);
     if (_webSocket.state() == QAbstractSocket::ConnectedState) {
-        _webSocket.sendTextMessage(QStringLiteral("{\"command\": \"fetch_history\"}"));
+        _webSocket.sendTextMessage(QStringLiteral("{\"request_type\": \"record_history\"}"));
     } else {
         qDebug() << "WebSocket is not connected!";
     }
@@ -68,12 +68,12 @@ void Application::processMessageReceived(const QString &message) {
             qDebug() << "Access Denied!";
             _window->showMessage("Access Denied");
 
-        } else if (jsonObject.contains("username") && jsonObject.contains("date") && jsonObject.contains("time")) {
-
-            QString username = jsonObject["username"].toString();
-            QString date = jsonObject["date"].toString();
-            QString time = jsonObject["time"].toString();
-            _window->updateUserDetails(username, date, time);
+        } else if (jsonObject.contains("info")) {
+            QJsonObject jsonData = jsonObject["info"].toObject();
+            QString rfid = jsonData["rfid_tag"].toString();
+            QString date = jsonData["date"].toString();
+            QString time = jsonData["time"].toString();
+            _window->updateUserDetails(rfid, date, time);
 
         } else if (jsonObject.contains("history") && jsonObject["history"].isArray()) {
 
@@ -83,7 +83,7 @@ void Application::processMessageReceived(const QString &message) {
             _history->show(historyData);
 
         } else {
-            qDebug() << "Unexpected JSON format received";
+            qDebug() << "Unexpected JSON format received"<< jsonObject;
         }
     } else {
         qDebug() << "Invalid or non-JSON message received:" << message;
